aeb40f54956fe76139d7bf39b1dcc30d3f82c240
Hoanh An <hoanhan@bennington.edu>
2018-05-10 22:38:11 -0400

    init hub

---
+++ hub.py
@@ -0,0 +1,33 @@
+"""
+    hub.py - PiHub server
+    Author: Hoanh An (hoanhan@bennington.edu)
+    Date: 05/10/18
+"""
+
+from flask import Flask, jsonify, request
+from time import time, ctime
+
+app = Flask(__name__)
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    """
+    Test endpoint.
+    """
+    if request.method == 'GET':
+        response = {
+            'timestamp': ctime(time()),
+            'status': 'ok'
+        }
+    elif request.method == 'POST':
+        response = {
+            'timestamp': ctime(time()),
+            'status': 'ok',
+            'data': request.get_json()
+        }
+
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run()
