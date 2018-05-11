d230a026ad7cfaebaf9b2e92faf710186b3b8acf
Hoanh An <hoanhan@bennington.edu>
2018-05-10 22:39:41 -0400

    add sample publisher program

---
+++ sample.py
@@ -0,0 +1,29 @@
+"""
+    sample.py - Sample code using Publisher to push sensor workshop
+    Author: Hoanh An (hoanhan@bennington.edu)
+    Date: 05/10/2018
+"""
+
+from time import time, ctime
+from publisher import Publisher
+
+if __name__ == '__main__':
+    # Sample sensor reading
+    sample_data = {
+        'id': 101,
+        'data': {
+            'temperature': {
+                'value': 25,
+                'unit': 'C'
+            },
+            'humidity': {
+                'value': 60,
+                'unit': '%'
+            }
+        }
+    }
+
+    worker = Publisher()
+
+    print(worker.get('test'))
+    print(worker.publish('test', sample_data))
