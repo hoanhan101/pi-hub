5de1509290033d45b43bd894c0d22958d579142c
Hoanh An <hoanhan@bennington.edu>
2018-05-23 13:53:59 +0000

    update Paulinia functions

--- sql/functions.py
+++ sql/functions.py
@@ -17,21 +17,30 @@ database = "sensor_workshop"
 #- get all temperature readings from sensor x
 #- get temperature readings from sensor x in the last hour
 #- get temperature readings from sensor x in yesterday
+#- get all sensor ids with their locations
+#- get all readings
+#- get all reading for a given sensor id in the last hour
+#- get all reading for a given sensor id from yesterday
+#- get max reading in the last hour
+#- get max reading from yesterday
+#- clean up the table (just for quick testing)
+
+# Due to some weird error with PDO, parameters will not be passed through placeholders at times.
 
 # Worth mentioning: SELECT statements return the full row and not just one column. Can edit if needed.
-
-from mysql.connector import errorcode
-try:
-  cnx = mysql.connector.connect(host, user, psswd, database)
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cnx.close()
+def test_connection():
+	from mysql.connector import errorcode
+	try:
+		cnx = mysql.connector.connect(host, user, psswd, database)
+	except mysql.connector.Error as err:
+  		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			print("Something is wrong with your user name or password")
+		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+			print("Database does not exist")
+		else:
+			print(err)
+	else:
+		cnx.close()
 
 
 
@@ -40,38 +49,115 @@ def connect():
     link = mysql.connector.connect(user = user, password= psswd, host = host, database = database)
     return link;
 
+
+#Insert reading
 def insert_reading(id, loc, temp, d, hum):
     link = connect()
     cursor = link.cursor()
     q = "INSERT INTO readings (sensor_id, location, temp, temp_degree, humidity, timestamp) VALUES (%s, %s, %s, %s, %s, NOW())"
     cursor.execute(q, (id, loc, temp, d, hum))
+    link.close()
 
-
+#Gets all readings from sensor
 def get_readings_from_sensor(id):
     link = connect()
     cursor = link.cursor()
     q = "SELECT sensor_id,temp,temp_degree,humidity, CAST(timestamp AS CHAR(30)) FROM readings WHERE sensor_id="+str(id)
     cursor.execute(q)
     info = cursor.fetchall()
-    return json.dumps(info);
+    link.close()
+    return info
 
 
+#Gets all readings from the day before
 def get_readings_yesterday():
     link = connect()
     cursor = link.cursor()
     q = "SELECT sensor_id,temp,temp_degree,humidity, CAST(timestamp AS CHAR(30)) FROM readings WHERE DATEDIFF(CURDATE(), DATE(timestamp))=1"
     cursor.execute(q)
     info = cursor.fetchall()
-    return json.dumps(info)
+    link.close()
+    return info
 
 
+#Gets all the readings from the last hour
 def get_readings_last_hour():
     link = connect()
     cursor = link.cursor()
     q = "SELECT sensor_id,temp,temp_degree,humidity, CAST(timestamp AS CHAR(30)) FROM readings WHERE TIME_TO_SEC(TIMEDIFF(NOW(), timestamp)) BETWEEN 0 AND 3600"
     cursor.execute(q)
     info = cursor.fetchall()
-    return json.dumps(info);
+    link.close()
+    return info
 
 
+#Gets all readings
+def get_all_readings():
+    link = connect()
+    cursor = link.cursor()
+    q = "SELECT sensor_id,temp,temp_degree,humidity, CAST(timestamp AS CHAR(30)) FROM readings "
+    cursor.execute(q)
+    info = cursor.fetchall()
+    link.close()
+    return info
 
+#Gets all readings from the day before by sensor id
+def get_readings_yesterday_by_id(id):
+    link = connect()
+    cursor = link.cursor()
+    q = "SELECT sensor_id,temp,temp_degree,humidity, CAST(timestamp AS CHAR(30)) FROM readings WHERE DATEDIFF(CURDATE(), DATE(timestamp))=1 AND sensor_id="+id
+    cursor.execute(q)
+    info = cursor.fetchall()
+    link.close()
+    return info
+
+
+#Gets all the readings from the last hour by sensor id
+def get_readings_last_hour_by_id(id):
+    link = connect()
+    cursor = link.cursor()
+    q = "SELECT sensor_id,temp,temp_degree,humidity, CAST(timestamp AS CHAR(30)) FROM readings WHERE (TIME_TO_SEC(TIMEDIFF(NOW(), timestamp)) BETWEEN 0 AND 3600) AND sensor_id="+id
+    cursor.execute(q)
+    info = cursor.fetchall()
+    link.close()
+    return info
+
+
+#Gets max reading from the day before
+def get_max_reading_yesterday():
+    link = connect()
+    cursor = link.cursor()
+    q = "SELECT sensor_id,MAX(temp),temp_degree,humidity, CAST(timestamp AS CHAR(30)) FROM readings WHERE DATEDIFF(CURDATE(), DATE(timestamp))=1 AND sensor_id="+id
+    cursor.execute(q)
+    info = cursor.fetchall()
+    link.close()
+    return info
+
+
+#Gets max readings from the last hour
+def get_max_reading_last_hour():
+    link = connect()
+    cursor = link.cursor()
+    q = "SELECT sensor_id,MAX(temp),temp_degree,humidity, CAST(timestamp AS CHAR(30)) FROM readings WHERE TIME_TO_SEC(TIMEDIFF(NOW(), timestamp)) BETWEEN 0 AND 3600"
+    cursor.execute(q)
+    info = cursor.fetchall()
+    link.close()
+    return info
+
+#Clear table
+def clear_table():
+    link = connect()
+    cursor = link.cursor()
+    q = "TRUNCATE readings IGNORE DELETE TRIGGERS DROP STORAGE"
+    cursor.execute(q)
+    link.close()
+
+#Get all location and sensors
+def get_sensor_and_location():
+    link = connect()
+    cursor = link.cursor()
+    q = "SELECT DISTINCT sensor_id, location FROM readings ORDER BY location"
+    cursor.execute(q)
+    info = cursor.fetchall()
+    link.close()
+    return info
