0cd5f8db296997b7b6d7c64ded3ccfd78678c18b
Hoanh An <hoanhan@bennington.edu>
2018-05-25 09:33:47 -0400

    clean up sql files

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `readings` (
-  `sensor_id` int(11) NOT NULL,
-  `location` int(11) NOT NULL,
-  `temp` double(11,5) NOT NULL,
-  `temp_degree` char(1) NOT NULL,
-  `humidity` double(11,5) NOT NULL,
-  `timestamp` datetime NOT NULL ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-COMMIT;
